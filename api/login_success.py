69c38c78ba26c303b3dd6e74699f709115147c69
yangtingting123456 <3048903923@qq.com>
2020-11-05 17:48:09 +0800

    httprunner 接口测试代码提交

---
+++ api/login_success.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_token():
+    host ='http://dev.commerce.juneyaokc.com:8081/member/login'
+    params_data = {
+        "name": "admin",
+        "password": "e10adc3949ba59abbe56e057f20f883e"
+
+    }
+    headres_data = {
+       " Content-Type": "application/json"
+    }
+    response = requests.post(url=host,json=params_data,headre = headres_data)
+    return response
+
+token = get_token()
+print(token)
